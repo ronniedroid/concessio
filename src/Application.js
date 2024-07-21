94d4fd318ffd5c6329692928ab88668b08413d5d
Ronnie Nissan <ronnie.nissan@proton.me>
2024-07-21 14:06:04 +0300

    initial commit: basic project structure

---
+++ src/Application.js
@@ -0,0 +1,13 @@
+import Adw from 'gi://Adw';
+import GObject from 'gi://GObject';
+
+import { CncWindow } from './Window.js'
+
+export const CncApplication = GObject.registerClass({
+    GTypeName: 'CncApplication'
+}, class extends Adw.Application {
+    vfunc_activate() {
+        const window = new CncWindow({ application: this });
+        window.present()
+    }
+})
---
+++ src/io.github.ronniedroid.concessio.js
@@ -0,0 +1,24 @@
+#!@GJS@ -m
+
+import GLib from 'gi://GLib';
+
+// Initialize the package
+imports.package.init({
+    name: '@PACKAGE_NAME@',
+    version: '@PACKAGE_VERSION@',
+    prefix: '@PREFIX@',
+    libdir: '@LIBDIR@',
+});
+
+// Import the main module and run the main function
+const loop = new GLib.MainLoop(null, false);
+import('resource:///io/github/ronniedroid/concessio/js/main.js')
+    .then((main) => {
+        GLib.idle_add(GLib.PRIORITY_DEFAULT_IDLE, () => {
+            loop.quit();
+            imports.package.run(main);
+            return GLib.SOURCE_REMOVE;
+        });
+    })
+    .catch(logError);
+loop.run();
