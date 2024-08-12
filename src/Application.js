9e7eeba0f0733fdce02194468f93c98baa4c511d
ronniedroid <ronnie.nissan@proton.me>
2024-08-12 19:11:51 +0300

    added about dialog

--- src/Application.js
+++ src/Application.js
@@ -2,6 +2,7 @@ import Adw from 'gi://Adw';
 import GObject from 'gi://GObject';
 import Gdk from 'gi://Gdk';
 import Gtk from 'gi://Gtk';
+import Gio from 'gi://Gio';
 
 import {CncForm} from './Form.js';
 import './BoxedList.js';
@@ -13,11 +14,18 @@ export const CncApplication = GObject.registerClass({
     vfunc_startup() {
 	super.vfunc_startup();
 	this.#loadStylesheet();
+        this.#setupActions();
     }
     
     vfunc_activate() {
         const window = new CncWindow({ application: this });
         window.present();
+    }
+
+    #setupActions() {
+        const aboutAction = new Gio.SimpleAction({name: 'about'});
+        aboutAction.connect("activate", () => this._openAboutDialog());
+        this.add_action(aboutAction);
     }
 
     #loadStylesheet() {
@@ -29,5 +37,25 @@ export const CncApplication = GObject.registerClass({
 	    provider,
 	    Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION
 	);
+    }
+
+    _openAboutDialog() {
+        const dialog = new Adw.AboutDialog({
+            application_icon: "io.github.ronniedroid.concessio",
+            application_name: "Concessio",
+            developer_name: "Ronnie Nissan",
+            version: pkg.version,
+            comments: (
+                "Typeset is an app that doesn’t exist and is used as an example content for About Dialog."
+            ),
+            website: "https://github.com/ronniedroid/concessio",
+            issue_url: "https://github.com/ronniedroid/concessio/issues",
+            copyright: "© 2024 Ronnie Nissan",
+            license_type: Gtk.License.GPL_3_0,
+            developers: ["Ronnie Nissan <ronnie.nissan@proton.me>"],
+            designers: ["Brage Fuglseth <bragefuglseth@gnome.org>"],
+        });
+
+        dialog.present(this.get_active_window());
     }
 });
--- src/main.js
+++ src/main.js
@@ -2,10 +2,10 @@ import 'gi://Gdk?version=4.0';
 import 'gi://Gtk?version=4.0';
 import 'gi://Adw?version=1';
 
-import { CncApplication } from './Application.js'
+import { CncApplication } from './Application.js';
 
 export function main(argv) {
     return new CncApplication({
-        'application-id': pkg.name
-    }).run(argv)
+        'application-id': pkg.name,
+    }).run(argv);
 }
